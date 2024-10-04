733d964713bad7da2eb28d4f1b631b8148e8c303
Tony Molina <pgraduacion.am2@gmail.com>
2024-10-03 21:15:33 -0600

    Temporizador de Ofertas

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Offer = () => {
   const calculateTimeLeft = () => {
-    const difference = +new Date("2024-08-12T12:15:00") - +new Date();
+    const difference = +new Date("2024-10-31T23:59:59") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -37,25 +37,25 @@ const Offer = () => {
   return (
     <div className="bg-banner bg-center bg-cover h-[555px] p-8 rounded-xl max-padd-container my-8">
       <div className="flex items-center flex-col gap-1 mt-40 max-w-xl">
-        <h3 className="uppercase medium-20">Sales Fever</h3>
-        <h2 className="bold-44 uppercase">20% Off Everything</h2>
-        <span className="italic font-ace">Offer ends in</span>
+        <h3 className="uppercase medium-20">20% de descuento</h3>
+        <h2 className="bold-44 uppercase">¡En toda la tienda!</h2>
+        <span className="italic font-ace">la oferta finaliza en</span>
           <div className="flex gap-x-7 mt-2">
             <div className="bg-white p-2 rounded-lg">
-              <span className="font-bold text-4xl">0{timeLeft.days}</span>
-              <span className="block">Days</span>
+              <span className="font-bold text-4xl">{timeLeft.days}</span>
+              <span className="block">Días</span>
             </div>
             <div className="bg-white p-2 rounded-lg">
               <span className="font-bold text-4xl">{timeLeft.hours}</span>
-              <span className="block">Hours</span>
+              <span className="block">Horas</span>
             </div>
             <div className="bg-white p-2 rounded-lg">
               <span className="font-bold text-4xl">{timeLeft.minutes}</span>
-              <span className="block">Minutes</span>
+              <span className="block">Minutos</span>
             </div>
             <div className="bg-white p-2 rounded-lg">
               <span className="font-bold text-4xl">{timeLeft.seconds}</span>
-              <span className="block">Seconds</span>
+              <span className="block">Segundos</span>
             </div>
           </div>
       </div>
